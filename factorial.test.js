f3ee2bd32cc90f7d70f22ef94f3ec2c6916b391b
unknown <avpanchi@espe.edu.ec>
2025-08-19 12:25:12 -0500

    Pruebas4

--- factorial.test.js
+++
@@ -1,5 +0,0 @@
-const fac = require('./math.js');
-
-test('Factorial of 0 is 1', () => {
-    expect(fac.factorial(0)).toBe(1);
-});
--- fibonacci.test.js
+++
@@ -1,8 +0,0 @@
-const fib = require('./math.js');
-
-test(
-    'Expect 13 when calculating the 7th Fibonacci number',
-    () => {
-        expect(fib.fibonacci(7)).toBe(13);
-    }
-);
